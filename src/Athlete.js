3b0905b7df987005e6a7e153234a8f0f9b0cd13e
irunninglog <allan@irunninglog.com>
2019-04-20 21:24:30 -0400

    #1 - Athlete data can be retrieved from the server

--- src/Athlete.js
+++ src/Athlete.js
@@ -5,6 +5,7 @@ class Athlete extends React.Component {
     render() {
         return (
             <div className="Athlete">
+                <div>Logged in athlete:</div>
                 <div>id: {this.props.athlete.id}</div>
                 <div>name: {this.props.athlete.firstName} {this.props.athlete.lastName}</div>
                 <div>location: {this.props.athlete.city}, {this.props.athlete.state}, {this.props.athlete.country}</div>
