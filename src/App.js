f126fa4b76b4d8e1e5683cf2f74dfc2dc119df76
irunninglog <allan@irunninglog.com>
2019-04-20 20:03:51 -0400

    #1 - Athlete data can be retrieved from the server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Athlete from './Athlete';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-class App extends Component {
+const AthleteQuery = () => (
+  <Query
+    query={gql`
+      {
+        athlete {
+          id
+          firstName
+          lastName
+          city
+          state
+          country
+          sex
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return (
+        <div>
+          <Athlete athlete={data.athlete}/>
+        </div>
+      );
+    }}
+  </Query>
+);
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          <AthleteQuery />
         </header>
       </div>
     );
---
+++ src/Athlete.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Athlete extends React.Component {
+
+    render() {
+        return (
+            <div className="Athlete">
+                <div>id: {this.props.athlete.id}</div>
+                <div>name: {this.props.athlete.firstName} {this.props.athlete.lastName}</div>
+                <div>location: {this.props.athlete.city}, {this.props.athlete.state}, {this.props.athlete.country}</div>
+                <div>sex: {this.props.athlete.sex}</div>
+            </div>
+        );
+    }
+
+}
+
+export default Athlete;
